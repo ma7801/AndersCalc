06a348d2426c859cd5ea45662dea5eca21aa7271
ma7801 <ma7801@gmail.com>
2018-02-28 20:39:53 -0800

    Initial Commit

---
+++ Assets/textOutputScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textOutputScript : MonoBehaviour {
+
+    public GameObject textOutput;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TextAppend()
+    {
+        textOutput = GameObject.Find("textOutput");
+
+        textOutput.GetComponent<>  ***STOPPED HERE 2-28-18
+    }
+}
